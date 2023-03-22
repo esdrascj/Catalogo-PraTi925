faa32ff454913f0e1182c96e3b3fec097ab539a1
Esdras Jesus <esdrasrubro@outlook.com>
2023-03-21 23:38:31 -0300

    commit com alterações no layout da home e menu

--- script.js
+++ script.js
@@ -1,39 +1,3 @@
-const botoesCategorias = document.querySelectorAll('.categoria');
-const produtos = document.querySelectorAll('.produto');
-const sliderContainer = document.querySelector('.slider-container');
-const sliderImages = document.querySelectorAll('.slider-container img');
-const sliderPrevButton = document.querySelector('#slider-prev');
-const sliderNextButton = document.querySelector('#slider-next');
-
-let currentIndex = 0;
-let slideInterval = setInterval(nextSlide, 5000);
-
-function nextSlide() {
-  if (currentIndex === sliderImages.length - 1) {
-    currentIndex = 0;
-  } else {
-    currentIndex++;
-  }
-  moveSlider();
-}
-
-function prevSlide() {
-  if (currentIndex === 0) {
-    currentIndex = sliderImages.length - 1;
-  } else {
-    currentIndex--;
-  }
-  moveSlider();
-}
-
-function moveSlider() {
-  sliderContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
-
-sliderPrevButton.addEventListener('click', prevSlide);
-sliderNextButton.addEventListener('click', nextSlide);
-
-
 botoesCategorias.forEach(botao => {
   botao.addEventListener('click', () => {
     // Remover classe active de todos os botões de categoria
